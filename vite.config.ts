287c24f20bb59b1f599cc77d5cf7b55910d2a01d
shufei.han <shufei.han@gl-inet.com>
2024-09-09 15:08:33 +0800

    feat: build测试

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
+/*
+ * @Author: shufei.han
+ * @Date: 2024-09-09 12:25:29
+ * @LastEditors: shufei.han
+ * @LastEditTime: 2024-09-09 15:08:12
+ * @FilePath: \xiaoyuanbao-names\vite.config.ts
+ * @Description: 
+ */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'docs'
+  }
 })
